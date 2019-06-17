bdcf8d121ee2975c5fe8a51968b78b11e136e713
Yoon Lee <himynameisyoon@gmail.com>
2019-06-17 13:42:36 -0700

    add class and first test

---
+++ __tests__/index.js
@@ -0,0 +1,9 @@
+const { Dollar } = require("../index");
+
+test("test multiplication", () => {
+  const five = new Dollar(5);
+
+  five.times(2);
+
+  expect(10, five.amount);
+});
---
+++ index.js
@@ -0,0 +1,5 @@
+class Dollar {
+  constructor() {}
+}
+
+module.exports = Dollar;
