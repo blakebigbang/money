95a0977af6fa672a39763c286ca09a85430ce737
Yoon Lee <himynameisyoon@gmail.com>
2019-06-17 17:49:13 -0700

    add rate pair class

---
+++ Pair.js
@@ -0,0 +1,18 @@
+class Pair {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+
+  equals(object) {
+    const pair = object;
+
+    return this.from === pair.from && this.to === pair.to;
+  }
+
+  hashCode() {
+    return 0;
+  }
+}
+
+module.exports = Pair;
